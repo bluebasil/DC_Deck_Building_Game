d47de7caba080fe4f831ba14dcd6241928d3e4fc
Bluebasil <emoreaua@uwaterloo.ca>
2026-03-06 22:34:58 -0500

    merge issue

--- frames/actions.py
+++ frames/actions.py
@@ -8,10 +8,6 @@ class special_action:
     click_action = None
     card = None
 
-	def __init__(self,button_text,click_action):
-		self.action_id = str(uuid.uuid4())
-		self.button_text = button_text
-		self.click_action = click_action
     def __init__(self, button_text, click_action, card=None):
         self.action_id = str(uuid.uuid4())
         self.button_text = button_text
